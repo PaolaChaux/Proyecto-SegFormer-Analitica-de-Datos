2fe99e777ae51832a9873b05743bd5dfa0840b58
lapiceroazul4 <lapiceroazul@proton.me>
2025-05-20 22:53:56 -0500

    get classes from model's config

--- appStreamlit.py
+++ appStreamlit.py
@@ -12,36 +12,16 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title="Demo SegFormer", layout="wide")
 
 @st.cache_resource
-def load_model():
+dpro    ef load_model():
     processor = SegformerImageProcessor.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512")
     model = SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512")
     return processor, model
 
 processor, model = load_model()
 
-classes = [
-    'background', 'wall', 'building', 'sky', 'floor', 'tree', 'ceiling', 'road',
-    'bed', 'windowpane', 'grass', 'cabinet', 'sidewalk', 'person', 'earth', 'door',
-    'table', 'mountain', 'plant', 'curtain', 'chair', 'car', 'water', 'painting',
-    'sofa', 'shelf', 'house', 'sea', 'mirror', 'rug', 'field', 'armchair', 'seat',
-    'fence', 'desk', 'rock', 'wardrobe', 'lamp', 'bathtub', 'railing', 'cushion',
-    'base', 'box', 'column', 'signboard', 'chest of drawers', 'counter', 'sand',
-    'sink', 'skyscraper', 'fireplace', 'refrigerator', 'grandstand', 'path',
-    'stairs', 'runway', 'case', 'pool table', 'pillow', 'screen door', 'stairway',
-    'river', 'bridge', 'bookcase', 'blind', 'coffee table', 'toilet', 'flower',
-    'book', 'hill', 'bench', 'countertop', 'stove', 'palm', 'kitchen island',
-    'computer', 'swivel chair', 'boat', 'bar', 'arcade machine', 'hovel', 'bus',
-    'towel', 'light', 'truck', 'tower', 'chandelier', 'awning', 'streetlight',
-    'booth', 'television receiver', 'airplane', 'dirt track', 'apparel', 'pole',
-    'land', 'bannister', 'escalator', 'ottoman', 'bottle', 'buffet', 'poster',
-    'stage', 'van', 'ship', 'fountain', 'conveyer belt', 'canopy', 'washer',
-    'plaything', 'swimming pool', 'stool', 'barrel', 'basket', 'waterfall',
-    'tent', 'bag', 'minibike', 'cradle', 'oven', 'ball', 'food', 'step', 'tank',
-    'trade name', 'microwave', 'pot', 'animal', 'bicycle', 'lake', 'dishwasher',
-    'screen', 'blanket', 'sculpture', 'hood', 'sconce', 'vase', 'traffic light',
-    'tray', 'ashcan', 'fan', 'pier', 'crt screen', 'plate', 'monitor', 'bulletin board',
-    'shower', 'radiator', 'glass', 'clock', 'flag'
-]
+
+classes = [model.config.id2label[i] for i in range(len(model.config.id2label))]
+
 
 def segment_image(image):
     inputs = processor(images=image, return_tensors="pt")
